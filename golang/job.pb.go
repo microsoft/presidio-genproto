226382f4144d30feff2a959a055950d275d4a287
ilana <ilanak@microsoft.com>
2018-07-23 13:54:11 +0300

    add job template

--- golang/job.pb.go
+++ golang/job.pb.go
@@ -18,167 +18,85 @@ var _ = math.Inf
 // proto package needs to be updated.
 const _ = proto.ProtoPackageIsVersion2 // please upgrade the proto package
 
-type Job struct {
-	Name                 string           `protobuf:"bytes,1,opt,name=name,proto3" json:"name,omitempty"`
-	Description          string           `protobuf:"bytes,2,opt,name=description,proto3" json:"description,omitempty"`
-	Trigger              *Trigger         `protobuf:"bytes,3,opt,name=trigger,proto3" json:"trigger,omitempty"`
-	ScannerTemplate      *ScannerTemplate `protobuf:"bytes,6,opt,name=scannerTemplate,proto3" json:"scannerTemplate,omitempty"`
-	XXX_NoUnkeyedLiteral struct{}         `json:"-"`
-	XXX_unrecognized     []byte           `json:"-"`
-	XXX_sizecache        int32            `json:"-"`
+type JobRequest struct {
+	Name                 string        `protobuf:"bytes,1,opt,name=name,proto3" json:"name,omitempty"`
+	Description          string        `protobuf:"bytes,2,opt,name=description,proto3" json:"description,omitempty"`
+	Trigger              *Trigger      `protobuf:"bytes,3,opt,name=trigger,proto3" json:"trigger,omitempty"`
+	ScanTemplate         *ScanTemplate `protobuf:"bytes,4,opt,name=scanTemplate,proto3" json:"scanTemplate,omitempty"`
+	XXX_NoUnkeyedLiteral struct{}      `json:"-"`
+	XXX_unrecognized     []byte        `json:"-"`
+	XXX_sizecache        int32         `json:"-"`
 }
 
-func (m *Job) Reset()         { *m = Job{} }
-func (m *Job) String() string { return proto.CompactTextString(m) }
-func (*Job) ProtoMessage()    {}
-func (*Job) Descriptor() ([]byte, []int) {
-	return fileDescriptor_job_8c36177fd988b50c, []int{0}
+func (m *JobRequest) Reset()         { *m = JobRequest{} }
+func (m *JobRequest) String() string { return proto.CompactTextString(m) }
+func (*JobRequest) ProtoMessage()    {}
+func (*JobRequest) Descriptor() ([]byte, []int) {
+	return fileDescriptor_job_e84309509186888c, []int{0}
 }
-func (m *Job) XXX_Unmarshal(b []byte) error {
-	return xxx_messageInfo_Job.Unmarshal(m, b)
+func (m *JobRequest) XXX_Unmarshal(b []byte) error {
+	return xxx_messageInfo_JobRequest.Unmarshal(m, b)
 }
-func (m *Job) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
-	return xxx_messageInfo_Job.Marshal(b, m, deterministic)
+func (m *JobRequest) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	return xxx_messageInfo_JobRequest.Marshal(b, m, deterministic)
 }
-func (dst *Job) XXX_Merge(src proto.Message) {
-	xxx_messageInfo_Job.Merge(dst, src)
+func (dst *JobRequest) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_JobRequest.Merge(dst, src)
 }
-func (m *Job) XXX_Size() int {
-	return xxx_messageInfo_Job.Size(m)
+func (m *JobRequest) XXX_Size() int {
+	return xxx_messageInfo_JobRequest.Size(m)
 }
-func (m *Job) XXX_DiscardUnknown() {
-	xxx_messageInfo_Job.DiscardUnknown(m)
+func (m *JobRequest) XXX_DiscardUnknown() {
+	xxx_messageInfo_JobRequest.DiscardUnknown(m)
 }
 
-var xxx_messageInfo_Job proto.InternalMessageInfo
+var xxx_messageInfo_JobRequest proto.InternalMessageInfo
 
-func (m *Job) GetName() string {
+func (m *JobRequest) GetName() string {
 	if m != nil {
 		return m.Name
 	}
 	return ""
 }
 
-func (m *Job) GetDescription() string {
+func (m *JobRequest) GetDescription() string {
 	if m != nil {
 		return m.Description
 	}
 	return ""
 }
 
-func (m *Job) GetTrigger() *Trigger {
+func (m *JobRequest) GetTrigger() *Trigger {
 	if m != nil {
 		return m.Trigger
 	}
 	return nil
 }
 
-func (m *Job) GetScannerTemplate() *ScannerTemplate {
+func (m *JobRequest) GetScanTemplate() *ScanTemplate {
 	if m != nil {
-		return m.ScannerTemplate
+		return m.ScanTemplate
 	}
 	return nil
-}
-
-type Trigger struct {
-	Schedule             *Schedule `protobuf:"bytes,1,opt,name=schedule,proto3" json:"schedule,omitempty"`
-	XXX_NoUnkeyedLiteral struct{}  `json:"-"`
-	XXX_unrecognized     []byte    `json:"-"`
-	XXX_sizecache        int32     `json:"-"`
-}
-
-func (m *Trigger) Reset()         { *m = Trigger{} }
-func (m *Trigger) String() string { return proto.CompactTextString(m) }
-func (*Trigger) ProtoMessage()    {}
-func (*Trigger) Descriptor() ([]byte, []int) {
-	return fileDescriptor_job_8c36177fd988b50c, []int{1}
-}
-func (m *Trigger) XXX_Unmarshal(b []byte) error {
-	return xxx_messageInfo_Trigger.Unmarshal(m, b)
-}
-func (m *Trigger) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
-	return xxx_messageInfo_Trigger.Marshal(b, m, deterministic)
-}
-func (dst *Trigger) XXX_Merge(src proto.Message) {
-	xxx_messageInfo_Trigger.Merge(dst, src)
-}
-func (m *Trigger) XXX_Size() int {
-	return xxx_messageInfo_Trigger.Size(m)
-}
-func (m *Trigger) XXX_DiscardUnknown() {
-	xxx_messageInfo_Trigger.DiscardUnknown(m)
-}
-
-var xxx_messageInfo_Trigger proto.InternalMessageInfo
-
-func (m *Trigger) GetSchedule() *Schedule {
-	if m != nil {
-		return m.Schedule
-	}
-	return nil
-}
-
-type Schedule struct {
-	RecurrencePeriodDuration string   `protobuf:"bytes,1,opt,name=recurrencePeriodDuration,proto3" json:"recurrencePeriodDuration,omitempty"`
-	XXX_NoUnkeyedLiteral     struct{} `json:"-"`
-	XXX_unrecognized         []byte   `json:"-"`
-	XXX_sizecache            int32    `json:"-"`
-}
-
-func (m *Schedule) Reset()         { *m = Schedule{} }
-func (m *Schedule) String() string { return proto.CompactTextString(m) }
-func (*Schedule) ProtoMessage()    {}
-func (*Schedule) Descriptor() ([]byte, []int) {
-	return fileDescriptor_job_8c36177fd988b50c, []int{2}
-}
-func (m *Schedule) XXX_Unmarshal(b []byte) error {
-	return xxx_messageInfo_Schedule.Unmarshal(m, b)
-}
-func (m *Schedule) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
-	return xxx_messageInfo_Schedule.Marshal(b, m, deterministic)
-}
-func (dst *Schedule) XXX_Merge(src proto.Message) {
-	xxx_messageInfo_Schedule.Merge(dst, src)
-}
-func (m *Schedule) XXX_Size() int {
-	return xxx_messageInfo_Schedule.Size(m)
-}
-func (m *Schedule) XXX_DiscardUnknown() {
-	xxx_messageInfo_Schedule.DiscardUnknown(m)
-}
-
-var xxx_messageInfo_Schedule proto.InternalMessageInfo
-
-func (m *Schedule) GetRecurrencePeriodDuration() string {
-	if m != nil {
-		return m.RecurrencePeriodDuration
-	}
-	return ""
 }
 
 func init() {
-	proto.RegisterType((*Job)(nil), "types.Job")
-	proto.RegisterType((*Trigger)(nil), "types.Trigger")
-	proto.RegisterType((*Schedule)(nil), "types.Schedule")
+	proto.RegisterType((*JobRequest)(nil), "types.JobRequest")
 }
 
-func init() { proto.RegisterFile("job.proto", fileDescriptor_job_8c36177fd988b50c) }
+func init() { proto.RegisterFile("job.proto", fileDescriptor_job_e84309509186888c) }
 
-var fileDescriptor_job_8c36177fd988b50c = []byte{
-	// 227 bytes of a gzipped FileDescriptorProto
+var fileDescriptor_job_e84309509186888c = []byte{
+	// 165 bytes of a gzipped FileDescriptorProto
 	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xe2, 0xe2, 0xcc, 0xca, 0x4f, 0xd2,
 	0x2b, 0x28, 0xca, 0x2f, 0xc9, 0x17, 0x62, 0x2d, 0xa9, 0x2c, 0x48, 0x2d, 0x96, 0xe2, 0x2b, 0x49,
-	0xcd, 0x2d, 0xc8, 0x49, 0x2c, 0x49, 0x85, 0x08, 0x2b, 0x2d, 0x67, 0xe4, 0x62, 0xf6, 0xca, 0x4f,
-	0x12, 0x12, 0xe2, 0x62, 0xc9, 0x4b, 0xcc, 0x4d, 0x95, 0x60, 0x54, 0x60, 0xd4, 0xe0, 0x0c, 0x02,
-	0xb3, 0x85, 0x14, 0xb8, 0xb8, 0x53, 0x52, 0x8b, 0x93, 0x8b, 0x32, 0x0b, 0x4a, 0x32, 0xf3, 0xf3,
-	0x24, 0x98, 0xc0, 0x52, 0xc8, 0x42, 0x42, 0x1a, 0x5c, 0xec, 0x25, 0x45, 0x99, 0xe9, 0xe9, 0xa9,
-	0x45, 0x12, 0xcc, 0x0a, 0x8c, 0x1a, 0xdc, 0x46, 0x7c, 0x7a, 0x60, 0x6b, 0xf4, 0x42, 0x20, 0xa2,
-	0x41, 0x30, 0x69, 0x21, 0x07, 0x2e, 0xfe, 0xe2, 0xe4, 0xc4, 0xbc, 0xbc, 0xd4, 0xa2, 0x10, 0xa8,
-	0x03, 0x24, 0xd8, 0xc0, 0x3a, 0xc4, 0xa0, 0x3a, 0x82, 0x51, 0x65, 0x83, 0xd0, 0x95, 0x2b, 0x99,
-	0x71, 0xb1, 0x43, 0x4d, 0x15, 0xd2, 0xe6, 0xe2, 0x28, 0x4e, 0xce, 0x48, 0x4d, 0x29, 0xcd, 0x81,
-	0x38, 0x98, 0xdb, 0x88, 0x1f, 0x6e, 0x0a, 0x44, 0x38, 0x08, 0xae, 0x40, 0xc9, 0x8d, 0x8b, 0x03,
-	0x26, 0x2a, 0x64, 0xc5, 0x25, 0x51, 0x94, 0x9a, 0x5c, 0x5a, 0x54, 0x94, 0x9a, 0x97, 0x9c, 0x1a,
-	0x90, 0x5a, 0x94, 0x99, 0x9f, 0xe2, 0x52, 0x5a, 0x94, 0x08, 0xf6, 0x1e, 0xc4, 0xe7, 0x38, 0xe5,
-	0x93, 0xd8, 0xc0, 0x01, 0x66, 0x0c, 0x08, 0x00, 0x00, 0xff, 0xff, 0xed, 0xc4, 0xeb, 0x88, 0x54,
-	0x01, 0x00, 0x00,
+	0xcd, 0x2d, 0xc8, 0x49, 0x2c, 0x49, 0x85, 0x08, 0x2b, 0x2d, 0x65, 0xe4, 0xe2, 0xf2, 0xca, 0x4f,
+	0x0a, 0x4a, 0x2d, 0x2c, 0x4d, 0x2d, 0x2e, 0x11, 0x12, 0xe2, 0x62, 0xc9, 0x4b, 0xcc, 0x4d, 0x95,
+	0x60, 0x54, 0x60, 0xd4, 0xe0, 0x0c, 0x02, 0xb3, 0x85, 0x14, 0xb8, 0xb8, 0x53, 0x52, 0x8b, 0x93,
+	0x8b, 0x32, 0x0b, 0x4a, 0x32, 0xf3, 0xf3, 0x24, 0x98, 0xc0, 0x52, 0xc8, 0x42, 0x42, 0x1a, 0x5c,
+	0xec, 0x25, 0x45, 0x99, 0xe9, 0xe9, 0xa9, 0x45, 0x12, 0xcc, 0x0a, 0x8c, 0x1a, 0xdc, 0x46, 0x7c,
+	0x7a, 0x60, 0xdb, 0xf4, 0x42, 0x20, 0xa2, 0x41, 0x30, 0x69, 0x21, 0x73, 0x2e, 0x9e, 0xe2, 0xe4,
+	0xc4, 0xbc, 0x10, 0xa8, 0x23, 0x24, 0x58, 0xc0, 0xca, 0x85, 0xa1, 0xca, 0x83, 0x91, 0xa4, 0x82,
+	0x50, 0x14, 0x26, 0xb1, 0x81, 0x9d, 0x6b, 0x0c, 0x08, 0x00, 0x00, 0xff, 0xff, 0xc1, 0x7a, 0x58,
+	0xdd, 0xd2, 0x00, 0x00, 0x00,
 }
--- python/template_pb2.py
+++ python/template_pb2.py
@@ -20,7 +20,7 @@ DESCRIPTOR = _descriptor.FileDescriptor(
   name='template.proto',
   package='types',
   syntax='proto3',
-  serialized_pb=_b('\n\x0etemplate.proto\x12\x05types\x1a\x0c\x63ommon.proto\"4\n\x0f\x41nalyzeTemplate\x12!\n\x06\x66ields\x18\x01 \x03(\x0b\x32\x11.types.FieldTypes\"\xb7\x01\n\x11\x41nonymizeTemplate\x12\x0c\n\x04name\x18\x01 \x01(\t\x12\x13\n\x0b\x64isplayName\x18\x02 \x01(\t\x12\x13\n\x0b\x64\x65scription\x18\x03 \x01(\t\x12\x12\n\ncreateTime\x18\x04 \x01(\t\x12\x14\n\x0cmodifiedTime\x18\x05 \x01(\t\x12@\n\x18\x66ieldTypeTransformations\x18\x06 \x03(\x0b\x32\x1e.types.FieldTypeTransformation\"k\n\x17\x46ieldTypeTransformation\x12!\n\x06\x66ields\x18\x01 \x03(\x0b\x32\x11.types.FieldTypes\x12-\n\x0etransformation\x18\x02 \x01(\x0b\x32\x15.types.Transformation\"v\n\x0eTransformation\x12\x10\n\x08newValue\x18\x01 \x01(\t\x12)\n\x0creplaceValue\x18\x02 \x01(\x0b\x32\x13.types.ReplaceValue\x12\'\n\x0bredactValue\x18\x03 \x01(\x0b\x32\x12.types.RedactValue\" \n\x0cReplaceValue\x12\x10\n\x08newValue\x18\x01 \x01(\t\"\r\n\x0bRedactValue\"K\n\nDatabinder\x12\x10\n\x08\x62indType\x18\x01 \x01(\t\x12\x18\n\x10\x63onnectionString\x18\x02 \x01(\t\x12\x11\n\ttableName\x18\x03 \x01(\t\";\n\x12\x44\x61tabinderTemplate\x12%\n\ndatabinder\x18\x01 \x03(\x0b\x32\x11.types.Databinder\"S\n\x11\x42lobStorageConfig\x12\x13\n\x0b\x61\x63\x63ountName\x18\x01 \x01(\t\x12\x12\n\naccountKey\x18\x02 \x01(\t\x12\x15\n\rcontainerName\x18\x03 \x01(\t\"S\n\x08S3Config\x12\x10\n\x08\x61\x63\x63\x65ssId\x18\x01 \x01(\t\x12\x11\n\taccessKey\x18\x02 \x01(\t\x12\x0e\n\x06region\x18\x03 \x01(\t\x12\x12\n\nbucketName\x18\x04 \x01(\t\"e\n\x0bInputConfig\x12\x33\n\x11\x62lobStorageConfig\x18\x01 \x01(\x0b\x32\x18.types.BlobStorageConfig\x12!\n\x08s3Config\x18\x02 \x01(\x0b\x32\x0f.types.S3Config\"\xfd\x01\n\x0fScannerTemplate\x12\x0c\n\x04kind\x18\x01 \x01(\t\x12\'\n\x0binputConfig\x18\x02 \x01(\x0b\x32\x12.types.InputConfig\x12\x16\n\x0eminProbability\x18\x03 \x01(\t\x12/\n\x0f\x61nalyzeTemplate\x18\x04 \x01(\x0b\x32\x16.types.AnalyzeTemplate\x12\x33\n\x11\x61nonymizeTemplate\x18\x05 \x01(\x0b\x32\x18.types.AnonymizeTemplate\x12\x35\n\x12\x64\x61tabinderTemplate\x18\x06 \x01(\x0b\x32\x19.types.DatabinderTemplateb\x06proto3')
+  serialized_pb=_b('\n\x0etemplate.proto\x12\x05types\x1a\x0c\x63ommon.proto\"4\n\x0f\x41nalyzeTemplate\x12!\n\x06\x66ields\x18\x01 \x03(\x0b\x32\x11.types.FieldTypes\"\xb7\x01\n\x11\x41nonymizeTemplate\x12\x0c\n\x04name\x18\x01 \x01(\t\x12\x13\n\x0b\x64isplayName\x18\x02 \x01(\t\x12\x13\n\x0b\x64\x65scription\x18\x03 \x01(\t\x12\x12\n\ncreateTime\x18\x04 \x01(\t\x12\x14\n\x0cmodifiedTime\x18\x05 \x01(\t\x12@\n\x18\x66ieldTypeTransformations\x18\x06 \x03(\x0b\x32\x1e.types.FieldTypeTransformation\"k\n\x17\x46ieldTypeTransformation\x12!\n\x06\x66ields\x18\x01 \x03(\x0b\x32\x11.types.FieldTypes\x12-\n\x0etransformation\x18\x02 \x01(\x0b\x32\x15.types.Transformation\"v\n\x0eTransformation\x12\x10\n\x08newValue\x18\x01 \x01(\t\x12)\n\x0creplaceValue\x18\x02 \x01(\x0b\x32\x13.types.ReplaceValue\x12\'\n\x0bredactValue\x18\x03 \x01(\x0b\x32\x12.types.RedactValue\" \n\x0cReplaceValue\x12\x10\n\x08newValue\x18\x01 \x01(\t\"\r\n\x0bRedactValue\"K\n\nDatabinder\x12\x10\n\x08\x62indType\x18\x01 \x01(\t\x12\x18\n\x10\x63onnectionString\x18\x02 \x01(\t\x12\x11\n\ttableName\x18\x03 \x01(\t\";\n\x12\x44\x61tabinderTemplate\x12%\n\ndatabinder\x18\x01 \x03(\x0b\x32\x11.types.Databinder\"S\n\x11\x42lobStorageConfig\x12\x13\n\x0b\x61\x63\x63ountName\x18\x01 \x01(\t\x12\x12\n\naccountKey\x18\x02 \x01(\t\x12\x15\n\rcontainerName\x18\x03 \x01(\t\"S\n\x08S3Config\x12\x10\n\x08\x61\x63\x63\x65ssId\x18\x01 \x01(\t\x12\x11\n\taccessKey\x18\x02 \x01(\t\x12\x0e\n\x06region\x18\x03 \x01(\t\x12\x12\n\nbucketName\x18\x04 \x01(\t\"e\n\x0bInputConfig\x12\x33\n\x11\x62lobStorageConfig\x18\x01 \x01(\x0b\x32\x18.types.BlobStorageConfig\x12!\n\x08s3Config\x18\x02 \x01(\x0b\x32\x0f.types.S3Config\"\xb3\x01\n\x0cScanTemplate\x12\x0c\n\x04kind\x18\x01 \x01(\t\x12\'\n\x0binputConfig\x18\x02 \x01(\x0b\x32\x12.types.InputConfig\x12\x16\n\x0eminProbability\x18\x03 \x01(\t\x12\x19\n\x11\x61nalyzeTemplateId\x18\x04 \x01(\t\x12\x1b\n\x13\x61nonymizeTemplateId\x18\x05 \x01(\t\x12\x1c\n\x14\x64\x61tabinderTemplateId\x18\x06 \x01(\t\"i\n\x0bJobTemplate\x12\x0c\n\x04name\x18\x01 \x01(\t\x12\x13\n\x0b\x64\x65scription\x18\x02 \x01(\t\x12\x1f\n\x07trigger\x18\x03 \x01(\x0b\x32\x0e.types.Trigger\x12\x16\n\x0escanTemplateId\x18\x06 \x01(\t\",\n\x07Trigger\x12!\n\x08schedule\x18\x01 \x01(\x0b\x32\x0f.types.Schedule\",\n\x08Schedule\x12 \n\x18recurrencePeriodDuration\x18\x01 \x01(\tb\x06proto3')
   ,
   dependencies=[common__pb2.DESCRIPTOR,])
 
@@ -473,52 +473,52 @@ _INPUTCONFIG = _descriptor.Descriptor(
 )
 
 
-_SCANNERTEMPLATE = _descriptor.Descriptor(
-  name='ScannerTemplate',
-  full_name='types.ScannerTemplate',
+_SCANTEMPLATE = _descriptor.Descriptor(
+  name='ScanTemplate',
+  full_name='types.ScanTemplate',
   filename=None,
   file=DESCRIPTOR,
   containing_type=None,
   fields=[
     _descriptor.FieldDescriptor(
-      name='kind', full_name='types.ScannerTemplate.kind', index=0,
+      name='kind', full_name='types.ScanTemplate.kind', index=0,
       number=1, type=9, cpp_type=9, label=1,
       has_default_value=False, default_value=_b("").decode('utf-8'),
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       options=None, file=DESCRIPTOR),
     _descriptor.FieldDescriptor(
-      name='inputConfig', full_name='types.ScannerTemplate.inputConfig', index=1,
+      name='inputConfig', full_name='types.ScanTemplate.inputConfig', index=1,
       number=2, type=11, cpp_type=10, label=1,
       has_default_value=False, default_value=None,
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       options=None, file=DESCRIPTOR),
     _descriptor.FieldDescriptor(
-      name='minProbability', full_name='types.ScannerTemplate.minProbability', index=2,
+      name='minProbability', full_name='types.ScanTemplate.minProbability', index=2,
       number=3, type=9, cpp_type=9, label=1,
       has_default_value=False, default_value=_b("").decode('utf-8'),
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       options=None, file=DESCRIPTOR),
     _descriptor.FieldDescriptor(
-      name='analyzeTemplate', full_name='types.ScannerTemplate.analyzeTemplate', index=3,
-      number=4, type=11, cpp_type=10, label=1,
-      has_default_value=False, default_value=None,
+      name='analyzeTemplateId', full_name='types.ScanTemplate.analyzeTemplateId', index=3,
+      number=4, type=9, cpp_type=9, label=1,
+      has_default_value=False, default_value=_b("").decode('utf-8'),
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       options=None, file=DESCRIPTOR),
     _descriptor.FieldDescriptor(
-      name='anonymizeTemplate', full_name='types.ScannerTemplate.anonymizeTemplate', index=4,
-      number=5, type=11, cpp_type=10, label=1,
-      has_default_value=False, default_value=None,
+      name='anonymizeTemplateId', full_name='types.ScanTemplate.anonymizeTemplateId', index=4,
+      number=5, type=9, cpp_type=9, label=1,
+      has_default_value=False, default_value=_b("").decode('utf-8'),
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       options=None, file=DESCRIPTOR),
     _descriptor.FieldDescriptor(
-      name='databinderTemplate', full_name='types.ScannerTemplate.databinderTemplate', index=5,
-      number=6, type=11, cpp_type=10, label=1,
-      has_default_value=False, default_value=None,
+      name='databinderTemplateId', full_name='types.ScanTemplate.databinderTemplateId', index=5,
+      number=6, type=9, cpp_type=9, label=1,
+      has_default_value=False, default_value=_b("").decode('utf-8'),
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       options=None, file=DESCRIPTOR),
@@ -535,7 +535,121 @@ _SCANNERTEMPLATE = _descriptor.Descriptor(
   oneofs=[
   ],
   serialized_start=969,
-  serialized_end=1222,
+  serialized_end=1148,
+)
+
+
+_JOBTEMPLATE = _descriptor.Descriptor(
+  name='JobTemplate',
+  full_name='types.JobTemplate',
+  filename=None,
+  file=DESCRIPTOR,
+  containing_type=None,
+  fields=[
+    _descriptor.FieldDescriptor(
+      name='name', full_name='types.JobTemplate.name', index=0,
+      number=1, type=9, cpp_type=9, label=1,
+      has_default_value=False, default_value=_b("").decode('utf-8'),
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      options=None, file=DESCRIPTOR),
+    _descriptor.FieldDescriptor(
+      name='description', full_name='types.JobTemplate.description', index=1,
+      number=2, type=9, cpp_type=9, label=1,
+      has_default_value=False, default_value=_b("").decode('utf-8'),
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      options=None, file=DESCRIPTOR),
+    _descriptor.FieldDescriptor(
+      name='trigger', full_name='types.JobTemplate.trigger', index=2,
+      number=3, type=11, cpp_type=10, label=1,
+      has_default_value=False, default_value=None,
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      options=None, file=DESCRIPTOR),
+    _descriptor.FieldDescriptor(
+      name='scanTemplateId', full_name='types.JobTemplate.scanTemplateId', index=3,
+      number=6, type=9, cpp_type=9, label=1,
+      has_default_value=False, default_value=_b("").decode('utf-8'),
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      options=None, file=DESCRIPTOR),
+  ],
+  extensions=[
+  ],
+  nested_types=[],
+  enum_types=[
+  ],
+  options=None,
+  is_extendable=False,
+  syntax='proto3',
+  extension_ranges=[],
+  oneofs=[
+  ],
+  serialized_start=1150,
+  serialized_end=1255,
+)
+
+
+_TRIGGER = _descriptor.Descriptor(
+  name='Trigger',
+  full_name='types.Trigger',
+  filename=None,
+  file=DESCRIPTOR,
+  containing_type=None,
+  fields=[
+    _descriptor.FieldDescriptor(
+      name='schedule', full_name='types.Trigger.schedule', index=0,
+      number=1, type=11, cpp_type=10, label=1,
+      has_default_value=False, default_value=None,
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      options=None, file=DESCRIPTOR),
+  ],
+  extensions=[
+  ],
+  nested_types=[],
+  enum_types=[
+  ],
+  options=None,
+  is_extendable=False,
+  syntax='proto3',
+  extension_ranges=[],
+  oneofs=[
+  ],
+  serialized_start=1257,
+  serialized_end=1301,
+)
+
+
+_SCHEDULE = _descriptor.Descriptor(
+  name='Schedule',
+  full_name='types.Schedule',
+  filename=None,
+  file=DESCRIPTOR,
+  containing_type=None,
+  fields=[
+    _descriptor.FieldDescriptor(
+      name='recurrencePeriodDuration', full_name='types.Schedule.recurrencePeriodDuration', index=0,
+      number=1, type=9, cpp_type=9, label=1,
+      has_default_value=False, default_value=_b("").decode('utf-8'),
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      options=None, file=DESCRIPTOR),
+  ],
+  extensions=[
+  ],
+  nested_types=[],
+  enum_types=[
+  ],
+  options=None,
+  is_extendable=False,
+  syntax='proto3',
+  extension_ranges=[],
+  oneofs=[
+  ],
+  serialized_start=1303,
+  serialized_end=1347,
 )
 
 _ANALYZETEMPLATE.fields_by_name['fields'].message_type = common__pb2._FIELDTYPES
@@ -547,10 +661,9 @@ _TRANSFORMATION.fields_by_name['redactValue'].message_type = _REDACTVALUE
 _DATABINDERTEMPLATE.fields_by_name['databinder'].message_type = _DATABINDER
 _INPUTCONFIG.fields_by_name['blobStorageConfig'].message_type = _BLOBSTORAGECONFIG
 _INPUTCONFIG.fields_by_name['s3Config'].message_type = _S3CONFIG
-_SCANNERTEMPLATE.fields_by_name['inputConfig'].message_type = _INPUTCONFIG
-_SCANNERTEMPLATE.fields_by_name['analyzeTemplate'].message_type = _ANALYZETEMPLATE
-_SCANNERTEMPLATE.fields_by_name['anonymizeTemplate'].message_type = _ANONYMIZETEMPLATE
-_SCANNERTEMPLATE.fields_by_name['databinderTemplate'].message_type = _DATABINDERTEMPLATE
+_SCANTEMPLATE.fields_by_name['inputConfig'].message_type = _INPUTCONFIG
+_JOBTEMPLATE.fields_by_name['trigger'].message_type = _TRIGGER
+_TRIGGER.fields_by_name['schedule'].message_type = _SCHEDULE
 DESCRIPTOR.message_types_by_name['AnalyzeTemplate'] = _ANALYZETEMPLATE
 DESCRIPTOR.message_types_by_name['AnonymizeTemplate'] = _ANONYMIZETEMPLATE
 DESCRIPTOR.message_types_by_name['FieldTypeTransformation'] = _FIELDTYPETRANSFORMATION
@@ -562,7 +675,10 @@ DESCRIPTOR.message_types_by_name['DatabinderTemplate'] = _DATABINDERTEMPLATE
 DESCRIPTOR.message_types_by_name['BlobStorageConfig'] = _BLOBSTORAGECONFIG
 DESCRIPTOR.message_types_by_name['S3Config'] = _S3CONFIG
 DESCRIPTOR.message_types_by_name['InputConfig'] = _INPUTCONFIG
-DESCRIPTOR.message_types_by_name['ScannerTemplate'] = _SCANNERTEMPLATE
+DESCRIPTOR.message_types_by_name['ScanTemplate'] = _SCANTEMPLATE
+DESCRIPTOR.message_types_by_name['JobTemplate'] = _JOBTEMPLATE
+DESCRIPTOR.message_types_by_name['Trigger'] = _TRIGGER
+DESCRIPTOR.message_types_by_name['Schedule'] = _SCHEDULE
 _sym_db.RegisterFileDescriptor(DESCRIPTOR)
 
 AnalyzeTemplate = _reflection.GeneratedProtocolMessageType('AnalyzeTemplate', (_message.Message,), dict(
@@ -642,12 +758,33 @@ InputConfig = _reflection.GeneratedProtocolMessageType('InputConfig', (_message.
   ))
 _sym_db.RegisterMessage(InputConfig)
 
-ScannerTemplate = _reflection.GeneratedProtocolMessageType('ScannerTemplate', (_message.Message,), dict(
-  DESCRIPTOR = _SCANNERTEMPLATE,
+ScanTemplate = _reflection.GeneratedProtocolMessageType('ScanTemplate', (_message.Message,), dict(
+  DESCRIPTOR = _SCANTEMPLATE,
   __module__ = 'template_pb2'
-  # @@protoc_insertion_point(class_scope:types.ScannerTemplate)
+  # @@protoc_insertion_point(class_scope:types.ScanTemplate)
   ))
-_sym_db.RegisterMessage(ScannerTemplate)
+_sym_db.RegisterMessage(ScanTemplate)
+
+JobTemplate = _reflection.GeneratedProtocolMessageType('JobTemplate', (_message.Message,), dict(
+  DESCRIPTOR = _JOBTEMPLATE,
+  __module__ = 'template_pb2'
+  # @@protoc_insertion_point(class_scope:types.JobTemplate)
+  ))
+_sym_db.RegisterMessage(JobTemplate)
+
+Trigger = _reflection.GeneratedProtocolMessageType('Trigger', (_message.Message,), dict(
+  DESCRIPTOR = _TRIGGER,
+  __module__ = 'template_pb2'
+  # @@protoc_insertion_point(class_scope:types.Trigger)
+  ))
+_sym_db.RegisterMessage(Trigger)
+
+Schedule = _reflection.GeneratedProtocolMessageType('Schedule', (_message.Message,), dict(
+  DESCRIPTOR = _SCHEDULE,
+  __module__ = 'template_pb2'
+  # @@protoc_insertion_point(class_scope:types.Schedule)
+  ))
+_sym_db.RegisterMessage(Schedule)
 
 
 # @@protoc_insertion_point(module_scope)
